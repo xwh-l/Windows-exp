04e7ceb1bf4fd88ad5038d4856d9326e5bce0881
xwh-l <104201473+xwh-l@users.noreply.github.com>
2022-05-09 10:09:05 +0800

    Add files via upload

---
+++ 第一次上机作业/WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "生产者数目：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "消费者数目：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 25);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "产品最大数目：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(143, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(156, 25);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(580, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Result：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(350, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "生产速率(s):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "消费速率(s):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(455, 41);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(156, 25);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "3";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(455, 82);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(156, 25);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(28, 181);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(687, 379);
+            this.listBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 577);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 第一次上机作业/WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+
+        int curCnt = 0;
+        int totalCnt = 0;
+        int remainedCnt = 0;
+        static Mutex mutex = new Mutex();
+        const int BUFFER_SIZE = 20;
+        static Semaphore produce = new Semaphore(BUFFER_SIZE, BUFFER_SIZE);
+        static Semaphore consume = new Semaphore(0, BUFFER_SIZE);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //生产者线程
+        private void produceItem(string name,int rate)
+        {
+            while (true)
+            {
+                while (!produce.WaitOne(10))
+                {
+                    Console.WriteLine(name + " wants to produce an item, but the buffer is full");
+                    update(name + " wants to produce an item, but the buffer is full");
+
+                }
+
+                mutex.WaitOne();
+                ++curCnt;
+                remainedCnt++;
+
+                //new Thread(() => { addlist1(name); }).Start();
+                this.listBox1.Invoke(new Action(() => {
+                    listBox1.Items.Add(name + " produces an item, totally " + curCnt + ", now there are " + remainedCnt + " items in the buffer");
+                }));
+                
+
+                Console.WriteLine(name + " produces an item, totally " + curCnt + ", now there are " + remainedCnt + " items in the buffer");                
+                mutex.ReleaseMutex();
+                consume.Release();
+                Thread.Sleep(rate * 1000);
+                //Thread.Sleep(1000/rate);
+                if (curCnt >= totalCnt)
+                {
+                    //Thread.CurrentThread.Abort();
+                    Console.WriteLine(name + "exit.");
+                    break;
+                }
+
+            }
+        }
+
+        
+        //消费者线程
+        private void consumeItem(string name, int rate)
+        {
+            while (true)
+            {
+                while (!consume.WaitOne(10))
+                {
+                    Console.WriteLine(name + " wants to consume an item, but the buffer is empty");
+                }
+                mutex.WaitOne();
+                remainedCnt--;
+                //new Thread(() => { addlist2(name); }).Start();
+                listBox1.Invoke(new Action(() => {
+                    listBox1.Items.Add(name + " consumes an item, now there are " + remainedCnt + " items in the buffer");
+                }));
+                
+                Console.WriteLine(name + " consumes an item, now there are " + remainedCnt + " items");
+                //update(name + " consumes an item, now there are " + remainedCnt + " items");
+                mutex.ReleaseMutex();
+                produce.Release();
+                //Thread.Sleep(1000/rate);
+                Thread.Sleep(rate * 1000);
+                if (curCnt >= totalCnt)
+                {
+                    //Thread.CurrentThread.Abort();
+                    Console.WriteLine(name + "exit.");
+                    break;
+                }
+            }
+        }
+        
+        void ProducerConsumer(int producerCnt, int consumerCnt, int productRate, int consumeRate
+    , int buffer_size, int _totalProductNum)
+        {
+            
+            clearComments();
+            showComment("begin of producer consumer problem");
+            totalCnt = _totalProductNum;
+
+            Thread[] producers = new Thread[producerCnt];
+            for (int i = 0; i < producerCnt; i++)
+            {
+                string name = "producer" + i;
+                producers[i] = new Thread(() => { produceItem(name, productRate); });
+                producers[i].IsBackground = true;
+                producers[i].Start();
+            }
+            Thread.Sleep(2000);
+
+            Thread[] consumers = new Thread[consumerCnt];
+            for (int i = 0; i < consumerCnt; i++)
+            {
+                string name = "consumer" + i;
+                consumers[i] = new Thread(() => { consumeItem(name, consumeRate); });
+                //consumers[i] = new Thread(() => { consumeItem("consumer" + i, consumeRate); });
+                consumers[i].IsBackground = true;
+                consumers[i].Start();
+            }
+
+            
+            Console.WriteLine("End of producer consumer problem");
+            
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pro_num = int.Parse(textBox1.Text);//the number of producers
+            int cos_num = int.Parse(textBox2.Text);//the number of cosumers
+
+            int warehouse_size = int.Parse(textBox3.Text);//the size of the warehouse
+
+            int pro_rate = int.Parse(textBox4.Text);
+            int cos_rate = int.Parse(textBox5.Text);
+
+            button1.Enabled = false;
+            ProducerConsumer(pro_num, cos_num, pro_rate, cos_rate, BUFFER_SIZE, warehouse_size);
+            button1.Enabled = true;
+
+        }
+        //定义回调
+        private delegate void updateDelegate(string comment);
+        public void update(string comment)
+        {
+            showComment(comment);
+            
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string name = (string)e.Argument;
+            listBox1.Items.Add(name + " produces an item, totally " + curCnt + ", now there are " + remainedCnt + " items in the buffer");
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string name = (string)e.Argument;
+            listBox1.Items.Add(name + " consumes an item, now there are " + remainedCnt + " items in the buffer");
+        }
+        private void clearComments()
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void showComment(string comment)
+        {
+            if (comment.Length==0)
+            {                
+                return;
+            }
+
+            listBox1.Items.Add(comment);
+        }
+    }
+}
+
